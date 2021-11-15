a6297de6b5987eaf261e5de24aff3bc11d0b6039
minced1 <jacagu477@gmail.com>
2021-11-15 19:43:57 +0100

    changed fixed values to user input values

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,51 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    speedrun();
+    let mut target_num_answer = String::new();
+    println!("What number would you like to speedrun?");
+    io::stdin()
+        .read_line(&mut target_num_answer)
+        .expect("Failed to read line");
+    let target_num_answer: i32 = target_num_answer
+        .trim()
+        .parse()
+        .expect("Input is not an integer");
+
+    let mut range_start_answer = String::new();
+    println!("What should be the lowest number?");
+    io::stdin()
+        .read_line(&mut range_start_answer)
+        .expect("Failed to read line");
+    let range_start_answer: i32 = range_start_answer
+        .trim()
+        .parse()
+        .expect("Input is not an integer");
+
+    let mut range_end_answer = String::new();
+    println!("What should be the highest number?");
+    io::stdin()
+        .read_line(&mut range_end_answer)
+        .expect("Failed to read line");
+    let range_end_answer: i32 = range_end_answer
+        .trim()
+        .parse()
+        .expect("Input is not an integer");
+
+    speedrun(target_num_answer, range_start_answer, range_end_answer)
+}
+
+fn speedrun(target_num: i32, range_start: i32, range_end: i32) {
+    let mut num: i32 = 0;
+    let mut attempts: i32 = 0;
+
+    //
+    while num != target_num {
+       num = rand::thread_rng().gen_range(range_start..range_end);
+       attempts = attempts+1;
+    }
+    println!("number, attempts: {}, {}", num, attempts);
+
+    //If user types in "y" the script calls itself with the same values
     println!("Run again? y/N");
     let mut answer = String::new();
     io::stdin()
@@ -12,17 +56,6 @@ fn main() {
     let answer = answer.trim();
 
     if answer == "y" {
-        main();
+        speedrun(target_num, range_start, range_end);
     }
-}
-
-fn speedrun() {
-    let mut num: i32 = 0;
-    let mut attempts: i32 = 0;
-
-    while num != 69420 {
-       num = rand::thread_rng().gen_range(10000..99999);
-       attempts = attempts+1;
-    }
-    println!("number, attempts: {}, {}", num, attempts);
 }
